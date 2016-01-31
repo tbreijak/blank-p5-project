bf6ff902e46867c27f17ed93499ee5d9fd6c57d5
tbreijak <ab6131@wayne.edu>
2016-01-31 13:48:45 -0500

    copied to new branch
    boat

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
-var setup = function () {
-  // your code goes here
-};
+function setup() {
+  createCanvas(800, 800);
+}
 
-var draw = function () {
-  // your code goes here
-};
+function draw() {
+ fill(100);
+ rect(0, 200, 800, 600);
+
+ fill(255);
+ arc(250, 240, 280, 280, 0, PI, TWO_PI, CHORD);
+
+ rect(230, 50, 10, 200);
+ triangle(240, 50, 300, 150, 240, 250);
+}
