da9135968592991f1a67065c6aeb4337b22befd4
tbreijak <ab6131@wayne.edu>
2016-01-31 13:52:21 -0500

    transfer car
    car

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
-var setup = function () {
-  // your code goes here
-};
+function setup() {
+  createCanvas(800, 800)
+}
 
-var draw = function () {
-  // your code goes here
-};
+function draw() {
+  rect(300, 400, 450, 100); // draw the body
+  quad(450, 350, 600, 350, 625, 400, 400, 400); // draw the top
+
+  ellipse(360, 500, 75, 75); // draw the front tire
+  ellipse(650, 500, 75, 75); // draw the rear tire
+
+  rect(725, 450, 30, 30); // draw tail light
+
+
+}
