e978a4eaf9af28a1f700e059f70935b713776eef
tbreijak <ab6131@wayne.edu>
2016-01-31 13:56:22 -0500

    copied house
    house

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
-var setup = function () {
-  // your code goes here
-};
+function setup() {
+  createCanvas(720, 700);
 
-var draw = function () {
-  // your code goes here
-};
+  fill(100);
+  rect(100, 200, 400, 400);
+
+  triangle(90, 200, 510, 200, 320, 10);
+
+  fill(200)
+  rect(275, 515, 50, 85);
+
+  fill(300)
+  ellipse(313, 560, 10, 10)
+
+  fill(200);
+  rect(500, 500, 200, 100);
+
+  fill(230)
+  rect(500, 400, 200, 100);
+
+  fill(300);
+  ellipse(310, 140, 75, 75);
+
+  fill(300);
+  rect(150, 250, 110, 90);
+  rect(350, 250, 110, 90);
+  rect(150, 380, 110, 90);
+  rect(350, 380, 110, 200);
+
+  fill(300)
+  line(350, 450, 460, 450);
+  line(405, 380, 405, 580)
+
+  ;
+}
+
+function draw() {
+
+}
