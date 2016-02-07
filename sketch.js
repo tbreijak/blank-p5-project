d3fb4f0ddbb153aba12f02572ff513b02551e6bc
tbreijak <ab6131@wayne.edu>
2016-02-07 15:21:45 -0500

    test
    test

--- sketch.js
+++ sketch.js
@@ -15,8 +15,8 @@ function drawRocket()  {
   var bodyY = 300;
   var bodyWidth = 200;
   var bodyHeight = 350;
-  var tipTopX = 600;
-  var tipTopY = 300;
+  var tipTopX = 400;
+  var tipTopY = 400;
   var topLeftX = 300;
   var topLeftY = 500;
   var outerLeftX = 250;
